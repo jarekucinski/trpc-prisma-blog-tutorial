c809cdfdf726270fcaeed4a51e419139b7c3f238
Jaroslaw Ucinski <jaroslaw.ucinski@cshark.com>
2022-08-20 20:54:01 +0200

    set up trpc

---
+++ src/server/createContext.ts
@@ -0,0 +1,7 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export function createContext({ req, res }: { req: NextApiRequest; res: NextApiResponse }) {
+  return { req, res }
+}
+
+export type Context = ReturnType<typeof createContext>
