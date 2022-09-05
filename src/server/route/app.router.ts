664e65982b4f0ceeaf0289fc45e258c0fdd13f32
Jarek Ucinski <jucinski@wp.pl>
2022-09-05 20:51:17 +0200

    add comments router to app router

--- src/server/route/app.router.ts
+++ src/server/route/app.router.ts
@@ -1,7 +1,11 @@
 import { createRouter } from '../createRouter'
+import { commentRouter } from './comment.router'
 import { postRouter } from './post.router'
 import { userRouter } from './user.router'
 
-export const appRouter = createRouter().merge('users.', userRouter).merge('posts.', postRouter)
+export const appRouter = createRouter()
+  .merge('users.', userRouter)
+  .merge('posts.', postRouter)
+  .merge('comments.', commentRouter)
 
 export type AppRouter = typeof appRouter
